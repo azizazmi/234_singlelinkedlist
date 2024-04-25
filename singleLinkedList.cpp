31866411dbf2f448e5958f041104d11ab61741ed
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-04-25 16:26:39 +0700

    memperbaiki line 51 dari "nim->noMHS" menjadi "nim > current->noMhs"

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -48,7 +48,7 @@ void addNode() {
 bool searchNode(int nim, Node* current, Node* previous) {
     previous = START;
     current = START;
-    while (current != NULL && nim->noMHS) {
+    while (current != NULL && nim > current->noMhs) {
         previous = current;
         current = current->next;
     }
